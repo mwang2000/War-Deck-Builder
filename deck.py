28929ed16316c2dabb6bae7119b07015e776f0b3
Maggie <maggiewang759@gmail.com>
2020-08-14 18:57:26 -0700

    added ability to ignore incorrect card names and check every possible deck in deckshop and rank them

---
+++ deck.py
@@ -0,0 +1,8 @@
+class Deck:
+    def __init__(self,cards,defense,offense,versatility,synergy):
+        self.cards = cards
+        self.defense = defense
+        self.offense = offense
+        self.versatility = versatility
+        self.synergy = synergy
+    
